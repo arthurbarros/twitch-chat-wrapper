57512c96c6ea7954d4f30a239f5d48e5fbfd7d15
Arthur Barros <arthbarros@gmail.com>
2017-12-31 14:22:12 -0200

    add pipy depency

--- setup.py
+++ setup.py
@@ -18,7 +18,9 @@ setup(
     platforms=["any"],
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[],
+    install_requires=[
+        'events'
+    ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
